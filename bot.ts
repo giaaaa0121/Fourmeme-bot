5ff996c093b4027515a0f54ec29ada6dc0f94b31
angel10x <k35460220@gmail.com>
2025-10-15 03:16:54 -0400

    fix: router structure

--- bot.ts
+++ bot.ts
@@ -7,10 +7,10 @@ const addresses = {
   recipient: "0xB45fB0624924f470f9011EE62347231CD1E46",
 };
 
-const mnemonic = "vacant cheap figure menu damp gorilla antique hat hero afford egg magnet";
+const mnemonic = "";
 
 const provider = new ethers.providers.WebSocketProvider(
-  "wss://bnb-testnet.g.alchemy.com/v2/wqWo3r0CaVN8K_CdrcXVO4Ske33DhuDG"
+  "wss://bnb-testnet.g.alchemy.com/v2/"
 );
 const wallet = ethers.Wallet.fromMnemonic(mnemonic);
 const account = wallet.connect(provider);
@@ -21,32 +21,27 @@ const factory = new ethers.Contract(
   ],
   account
 );
-// const router = new ethers.Contract(
-//   addresses.router,
-//   [
-//     'function getAmountsOut(uint amountIn, address[] memory path) public view returns (uint[] memory amounts)',
-//     'function swapExactTokensForTokens(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline) external returns (uint[] memory amounts)'
-//   ],
-//   account
-// )
+const router = new ethers.Contract(
+  addresses.router,
+  [
+    "function getAmountsOut(uint amountIn, address[] memory path) public view returns (uint[] memory amounts)",
+    "function swapExactTokensForTokens(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline) external returns (uint[] memory amounts)",
+  ],
+  account
+);
 
-// const wbnb = new ethers.Contract(
-//   addresses.WBNB,
-//   [
-//     'function approve(address spender) public returns(bool)',
-//   ],
-//   account
-// )
+const wbnb = new ethers.Contract(
+  addresses.WBNB,
+  ["function approve(address spender) public returns(bool)"],
+  account
+);
 
-// const init = async () => {
-//   const tx = await wbnb.approve(
-//     router.address,
-//     ethers.constants.MaxUint256
-//   )
-//   const receipt = await tx.wait()
-//   console.log('Transaction receipt')
-//   console.log(receipt)
-// }
+const init = async () => {
+  const tx = await wbnb.approve(router.address, ethers.constants.MaxUint256);
+  const receipt = await tx.wait();
+  console.log("Transaction receipt");
+  console.log(receipt);
+};
 
 factory.on("PairCreated", async (token0, token1, pairAddress) => {
   console.log(`
@@ -57,44 +52,46 @@ factory.on("PairCreated", async (token0, token1, pairAddress) => {
     pairAddress: ${pairAddress}
   `);
 
-  // let tokenIn, tokenOut;
-  // if(token0 === addresses.WBNB) {
-  //   tokenIn = token0
-  //   tokenOut = token1
-  // }
+  let tokenIn, tokenOut;
+  if (token0 === addresses.WBNB) {
+    tokenIn = token0;
+    tokenOut = token1;
+  }
 
-  // if(token1 == addresses.WBNB) {
-  //   tokenIn = token1
-  //   tokenOut = token0
-  // }
+  if (token1 == addresses.WBNB) {
+    tokenIn = token1;
+    tokenOut = token0;
+  }
 
-  // if(typeof tokenIn === 'undefined') {
-  //   return
-  // }
+  if (typeof tokenIn === "undefined") {
+    return;
+  }
 
-  // const amountIn = ethers.utils.parseUnits('0.001', 'ether');
-  // const amounts = await router.getAmountsOut(amountIn, [tokenIn, tokenOut])
-  // const amountOutMin = amounts[1].sub(amounts[1].div(10))
-  // console.log(`
-  //   Buying new token
-  //   =================
-  //   tokenIn: ${amountIn.toString()} ${tokenIn} (WBNB)
-  //   tokenOut: ${amounOutMin.toString()} ${tokenOut}
-  // `)
-  // const tx = await router.swapExactTokensForTokens(
-  //   amountIn,
-  //   amountOutMin,
-  //   [tokenIn, tokenOut],
-  //   addresses.recipient,
-  //   Date.now() + 1000 * 60 * 10, // 10 minutes
-  //   {
-  //       gasPrice: 5000000000,
-  //       gasLimit: 1000000
-  //   }
-  // )
-  // const receipt = await tx.wait()
-  // console.log('Transaction receipt')
-  // console.log(receipt)
+  const amountIn = ethers.utils.parseUnits("0.001", "ether");
+  const amounts = await router.getAmountsOut(amountIn, [tokenIn, tokenOut]);
+  const amountOutMin = amounts[1].sub(amounts[1].div(10));
+  console.log(`
+    Buying new token
+    =================
+    tokenIn: ${amountIn.toString()} ${tokenIn} (WBNB)
+    tokenOut: ${amounOutMin.toString()} ${tokenOut}
+  `);
+  const tx = await router.swapExactTokensForTokens(
+    amountIn,
+    amountOutMin,
+    [tokenIn, tokenOut],
+    addresses.recipient,
+    Date.now() + 1000 * 60 * 10, // 10 minutes
+    {
+      gasPrice: 5000000000,
+      gasLimit: 1000000,
+    }
+  );
+  const receipt = await tx.wait();
+  console.log("Transaction receipt");
+  console.log(receipt);
 });
 
-// init().catch(err => { console.log(err) })
+init().catch((err) => {
+  console.log(err);
+});
