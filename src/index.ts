1e80b9cdf4de0ff511d0dd589cbbf9486f9dddaa
angel10x <k35460220@gmail.com>
2025-10-15 03:37:44 -0400

    feat: added erc20 abi

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,13 @@ import { Command } from "commander";
 
 const program = new Command();
 
+console.log("Bot is started----->")
+
 program
   .name("fourdotmeme-bot")
   .description("Four.meme (BNB) toolkit CLI")
   .version("1.0.3");
 
-await program!.parseAsync(process.argv);
+// Volume bot
+
+program!.parseAsync(process.argv);
